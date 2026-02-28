65f30d471fbc1b948cccec17a7777803650e1c0e
SACHIN BURNWAL <sachinburnwal2004@gmail.com>
2026-02-28 22:57:10 +0530

    Time: 47 ms (55.03%) | Memory: 8.3 MB (17.45%) - LeetSync

--- 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
     int concatenatedBinary(int n) {
-        const int MOD = 1e9 + 7;
-        long long result = 0;
-        int bits = 0;
-
-        for (int i = 1; i <= n; i++) {
-            
-            // If i is power of 2 → increase bit length
-            if ((i & (i - 1)) == 0) {
-                bits++;
-            }
-
-            // Shift result and add i
-            result = ((result << bits) % MOD + i) % MOD;
+        int M = 1e9 + 7;
+        long result = 0;
+        for(int i=1; i<=n; i++){
+            int digits = log2(i) + 1;
+            result = ((result << digits)%M + i)%M;
         }
 
         return result;
