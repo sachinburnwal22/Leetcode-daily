0e3eed4fd69ee62615974ed4ebda905bd17ff8c4
SACHIN BURNWAL <sachinburnwal2004@gmail.com>
2026-03-05 23:16:46 +0530

    Time: 3 ms (39.29%) | Memory: 16.7 MB (16.04%) - LeetSync

--- 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -1,21 +1,8 @@
 class Solution {
 public:
     int minPartitions(string n) {
-        int cnt = 0;
-        while(true){
-            bool change = false;
-            for(char &ch : n){
-                if(ch != '0'){
-                    ch--;
-                    change = true;
-                }
-            }
-            if(!change){
-                break;
-            }
-            cnt++;
-        }
+        char maxChar = *max_element(begin(n), end(n));
 
-        return cnt;
+        return maxChar - '0';
     }
 };
