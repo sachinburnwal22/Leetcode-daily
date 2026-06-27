164e8ecc98cf1817f831ff25e95f874002755c4b
SACHIN BURNWAL <sachinburnwal2004@gmail.com>
2026-06-27 22:57:36 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (98.75%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -2,16 +2,20 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
+        // int i = 0;
         int j = 0;
         for(int i=0; i<n; i++){
-            if(nums[i] != 0){
+            if(nums[i] == 0){
+                continue;
+            }else{
                 nums[j] = nums[i];
                 j++;
             }
         }
 
-        for(int i=j; i<n; i++){
-            nums[i] = 0;
+        while(j < n){
+            nums[j] = 0;
+            j++;
         }
     }
 };
