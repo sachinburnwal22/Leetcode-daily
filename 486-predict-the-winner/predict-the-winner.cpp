837f090728c8c74bca9accd1c7d0d3ce4c380782
SACHIN BURNWAL <sachinburnwal2004@gmail.com>
2026-08-02 14:49:50 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (85.04%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
+    int t[21][21];
     int solve(int i, int j, vector<int>& nums){
         if(i > j) return 0;
-
+        if(t[i][j] != -1){
+            return t[i][j];
+        } 
         int takeI = nums[i] + min(solve(i+2, j, nums), solve(i+1, j-1, nums));
         int takeJ = nums[j] + min(solve(i+1, j-1, nums), solve(i, j-2, nums));
 
-        return max(takeI, takeJ);
+        return t[i][j] = max(takeI, takeJ);
     }
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
         int sum = accumulate(begin(nums), end(nums), 0);
-
+        memset(t, -1, sizeof(t));
         int p1 = solve(0, n-1, nums);
         int p2 = sum - p1;
         return p1 >= p2;
